644626185f5bd02bdd3cda994a1a928f8cc3755b
LiRen Tu <tuliren@gmail.com>
2022-02-19 23:21:53 -0800

    Update html-to-text version (#1)
    * Update to html-to-text 8.1
    
    * Remove item prefix
    
    * Use new config syntax
    
    * Try hyphen
    
    * Update selectors

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const htmlToText = require('html-to-text');
+const { convert } = require('html-to-text');
 (function(){
     const sanitize = function (post) {
-        const content = htmlToText.fromString(
-            post,
-            {
-                ignoreImage: true,
-                ignoreHref: true,
-                wordwrap: false,
-                uppercaseHeadings: false
+        const content = convert(
+            post,{
+              selectors: [
+                { selector: 'img', format: 'skip' },
+                { selector: 'a', options: { ignoreHref: true } },
+                { selector: 'ul', options: { itemPrefix: ' ' } },
+                { selector: 'h1', options: { uppercase: false } },
+                { selector: 'h2', options: { uppercase: false } },
+                { selector: 'h3', options: { uppercase: false } },
+                { selector: 'h4', options: { uppercase: false } },
+                { selector: 'h5', options: { uppercase: false } },
+                { selector: 'h6', options: { uppercase: false } },
+                { selector: 'table', options: { uppercaseHeaderCells: false } }
+              ]
             }
         );
         return content;
     }
 
     hexo.extend.filter.register('after_post_render', function (data) {
-        const excerptLength = hexo.config.excerpt_length || 300;
+        const excerptLength = hexo.config.excerpt_length || 150;
         const post = sanitize(data.content);
         const excerpt = post.substr(0, excerptLength);
         data.excerpt = excerpt;
