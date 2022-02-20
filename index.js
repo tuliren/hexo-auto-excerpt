f2fe1b771bdcddecd0f1a80ffeb343a91fc3b13f
Liren Tu <tuliren.git@outlook.com>
2022-02-19 23:20:32 -0800

    Update selectors

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ const { convert } = require('html-to-text');
               selectors: [
                 { selector: 'img', format: 'skip' },
                 { selector: 'a', options: { ignoreHref: true } },
-                { selector: 'ul', options: { itemPrefix: '-' } }
-              ],
-              wordwrap: 150,
-              uppercaseHeadings: false
+                { selector: 'ul', options: { itemPrefix: ' ' } },
+                { selector: 'h1', options: { uppercase: false } },
+                { selector: 'h2', options: { uppercase: false } },
+                { selector: 'h3', options: { uppercase: false } },
+                { selector: 'h4', options: { uppercase: false } },
+                { selector: 'h5', options: { uppercase: false } },
+                { selector: 'h6', options: { uppercase: false } },
+                { selector: 'table', options: { uppercaseHeaderCells: false } }
+              ]
             }
         );
         return content;
     }
 
     hexo.extend.filter.register('after_post_render', function (data) {
-        const excerptLength = hexo.config.excerpt_length || 300;
+        const excerptLength = hexo.config.excerpt_length || 150;
         const post = sanitize(data.content);
         const excerpt = post.substr(0, excerptLength);
         data.excerpt = excerpt;
